d0132e04c69399b217d4ffba2d7a76e885fb0264
JEONGAH LEE <65378914+jjeongah@users.noreply.github.com>
2021-11-15 11:30:22 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-//for javascript and jquery
+/*for calendar in main.html */
+const date = new Date();
+const viewYear = date.getFullYear();
+const viewMonth = date.getMonth();
+const prevDay = new Date(viewYear, viewMonth,0);
+const thisDay = new Date(viewYear, viewMonth + 1, 0);
+const Pdate = prevDay.getDate();
+const Pday = prevDay.getDay();
+const Tdate = thisDay.getDate();
+const Tday = thisDay.getDay();
+const prevDates = [];
+const thisDates = [...Array(Tdate + 1).keys()].slice(1);
+const nextDates= [];
+
+document.querySelector('.year-month').textContent = `${viewYear}년 ${viewMonth + 1}월`;
+
+if (Pday !== 6) {
+    for (let i = 0; i < Pday + 1; i++){
+        prevDates.unshift(Pdate-i);
+    }
+}
+
+for (let i = 1; i < 7 - Tday; i++){
+    nextDates.push(i);
+}
+const dates = prevDates.concat(thisDates, nextDates);
+
+dates.forEach((date, i) => {
+    dates[i] = `<div class="date">${date}</div>`;
+})
+
+document.querySelector('.dates').innerHTML = dates.join('');
+/*for calendar in main.html end */
